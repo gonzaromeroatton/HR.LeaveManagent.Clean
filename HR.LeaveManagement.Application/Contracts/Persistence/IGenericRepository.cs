8dcc5ea94e356fa6cf9bd0c2793c7fd97c63bf1a
Gonza Romero Atton <148910080+gonzaromeroatton@users.noreply.github.com>
2025-11-10 10:05:36 -0300

    Añadido proyecto Application y repositorios genéricos
    Se actualizó el archivo de solución `HR.LeaveManagent.Clean.sln` para incluir el nuevo proyecto `HR.LeaveManagement.Application`.
    Se creó el archivo `HR.LeaveManagement.Application.csproj` con configuración para .NET 7.0 y referencia al proyecto `HR.LeaveManagent.Domain`.
    
    Se implementó la interfaz genérica `IGenericRepository<T>` con métodos CRUD básicos.
    Se añadieron las interfaces específicas `ILeaveTypeRepository`, `ILeaveTAllocationRepository` e `ILeaveRequestRepository` para manejar entidades relacionadas con tipos, asignaciones y solicitudes de permisos.

---
+++ HR.LeaveManagement.Application/Contracts/Persistence/IGenericRepository.cs
@@ -0,0 +1,27 @@
+﻿using HR.LeaveManagent.Domain;
+
+namespace HR.LeaveManagement.Application.Contracts.Persistence;
+
+public interface IGenericRepository<T> where T : class
+{
+    Task<T> GetByIdAsync(int id);
+    Task<IReadOnlyList<T>> GetAllAsync();
+    Task<T> CreateAsync(T entity);
+    Task UpdateAsync(T entity);
+    Task DeleteAsync(T entity);
+}
+
+public interface ILeaveTypeRepository : IGenericRepository<LeaveType>
+{
+
+}
+
+public interface ILeaveTAllocationRepository : IGenericRepository<LeaveAllocation>
+{
+
+}
+
+public interface ILeaveRequestRepository : IGenericRepository<LeaveRequest>
+{
+
+}
