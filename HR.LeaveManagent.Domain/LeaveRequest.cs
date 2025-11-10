c7835995fbc27228ae7885edc0202a8151a2f8ed
Gonza Romero Atton <148910080+gonzaromeroatton@users.noreply.github.com>
2025-11-10 09:35:12 -0300

    Agregar archivos de proyecto.

---
+++ HR.LeaveManagent.Domain/LeaveRequest.cs
@@ -0,0 +1,16 @@
+﻿using HR.LeaveManagent.Domain.Common;
+
+namespace HR.LeaveManagent.Domain;
+
+public class LeaveRequest : BaseEntity
+{
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+    public LeaveType? LeaveType { get; set; }
+    public int LeaveTypeId { get; set; }
+    public DateTime DateRequested { get; set; }
+    public string? RequestComments { get; set; }
+    public bool? Approved { get; set; }
+    public bool Cancelled { get; set; }
+    public string RequestingEmployeeId { get; set; } = string.Empty;
+}
---
+++ HR.LeaveManagent.Domain/LeaveType.cs
@@ -0,0 +1,9 @@
+﻿using HR.LeaveManagent.Domain.Common;
+
+namespace HR.LeaveManagent.Domain;
+
+public class LeaveType : BaseEntity
+{
+    public string Name { get; set; } = string.Empty;
+    public int DefaultDays { get; set; }
+}
