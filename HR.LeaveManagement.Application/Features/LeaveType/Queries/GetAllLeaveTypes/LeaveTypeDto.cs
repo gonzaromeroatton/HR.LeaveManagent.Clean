31092b38641673c83675eab324d03246ebeac749
Gonza Romero Atton <148910080+gonzaromeroatton@users.noreply.github.com>
2025-11-10 11:34:47 -0300

    Actualización de dependencias y soporte para LeaveType
    Se realizaron los siguientes cambios:
    - Actualización de `AutoMapper` (v12.0.0 a v15.1.0) y `MediatR` (v11.1.0 a v13.1.0).
    - Configuración de `AutoMapper` con clave de licencia y carga de ensamblajes.
    - Registro de servicios de `MediatR` con `RegisterServicesFromAssembly`.
    - Creación de `LeaveTypeDto` para gestionar tipos de permisos.
    - Creación de `LeaveTypeProfile` para mapeo bidireccional entre `LeaveTypeDto` y `LeaveType`.
    - Eliminación de referencias a extensiones de `AutoMapper` y `MediatR`.
    - Adición de carpeta para comandos en `Features\LeaveType\Commands`.

---
+++ HR.LeaveManagement.Application/Features/LeaveType/Queries/GetAllLeaveTypes/LeaveTypeDto.cs
@@ -0,0 +1,10 @@
+﻿using HR.LeaveManagent.Domain.Common;
+
+namespace HR.LeaveManagement.Application.Features.LeaveType.Queries.GetAllLeaveTypes;
+
+public sealed class LeaveTypeDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public int DefaultDays { get; set; }
+}
