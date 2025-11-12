b44e623a8fb7f0bc449c2dd559a42fa8241026cf
Gonza Romero Atton <148910080+gonzaromeroatton@users.noreply.github.com>
2025-11-12 11:00:22 -0300

    Añadido proyecto de persistencia y mejoras en repositorio
    Se actualizó la interfaz `IGenericRepository` para que el método
    `CreateAsync` ya no devuelva un valor (`Task` en lugar de `Task<T>`).
    
    Se agregó el proyecto `HR.LeaveManagement.Persistence` con soporte
    para .NET 7.0, Entity Framework Core y SQL Server. Se incluyó la
    configuración de `DbContext` en `PersistenceServiceRegistration`.
    
    Se añadieron las clases `HrDatabaseContext` y `LeaveTypeConfiguration`
    para gestionar entidades y configuraciones iniciales.
    
    Se implementó `GenericRepository` con soporte básico para operaciones
    CRUD, dejando algunos métodos como `NotImplementedException`.

--- HR.LeaveManagement.Application/Contracts/Persistence/IGenericRepository.cs
+++ HR.LeaveManagement.Application/Contracts/Persistence/IGenericRepository.cs
@@ -6,7 +6,7 @@ public interface IGenericRepository<T> where T : class
 {
     Task<T> GetByIdAsync(int id);
     Task<IReadOnlyList<T>> GetAllAsync();
-    Task<T> CreateAsync(T entity);
+    Task CreateAsync(T entity);
     Task UpdateAsync(T entity);
     Task DeleteAsync(T entity);
 }
---
+++ HR.LeaveManagement.Persistence/Repositories/GenericRepository.cs
@@ -0,0 +1,40 @@
+﻿using HR.LeaveManagement.Application.Contracts.Persistence;
+using HR.LeaveManagement.Persistence.DatabaseContext;
+
+namespace HR.LeaveManagement.Persistence.Repositories;
+
+public class GenericRepository<T> : IGenericRepository<T> where T : class
+{
+    protected readonly HrDatabaseContext _context;
+
+    public GenericRepository(HrDatabaseContext context)
+    {
+        this._context = context;
+    }
+
+    public async Task CreateAsync(T entity)
+    {
+        await this._context.AddAsync(entity);
+        await this._context.SaveChangesAsync();
+    }
+
+    public Task DeleteAsync(T entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IReadOnlyList<T>> GetAllAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<T> GetByIdAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateAsync(T entity)
+    {
+        throw new NotImplementedException();
+    }
+}
