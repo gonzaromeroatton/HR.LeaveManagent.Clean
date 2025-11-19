9c4510d155ee2e3fd755ed5faeb090695519b43b
Gonza Romero Atton <148910080+gonzaromeroatton@users.noreply.github.com>
2025-11-19 20:22:01 -0300

    Añadidas funcionalidades CRUD para LeaveRequest
    Se agregó soporte para comandos y manejadores relacionados con
    la eliminación y actualización de solicitudes de permisos
    (`DeleteLeaveRequestCommand` y `UpdateLeaveRequestCommand`),
    incluyendo validadores y DTOs asociados.
    
    Se implementaron consultas para obtener detalles y listas de
    solicitudes de permisos (`GetLeaveRequestDetailsQuery` y
    `GetLeaveRequestListQuery`), junto con sus manejadores y DTOs.
    
    Se añadió la clase base `BaseLeaveRequest` y su validador
    `BaseLeaveRequestValidator` para estandarizar las propiedades
    y validaciones comunes de las solicitudes de permisos.
    
    Se actualizó `LeaveAllocationProfile.cs` para incluir el
    comando `UpdateLeaveAllocation`.

---
+++ HR.LeaveManagement.Application/Features/LeaveRequest/Shared/BaseLeaveRequestValidator.cs
@@ -0,0 +1,33 @@
+﻿using FluentValidation;
+using HR.LeaveManagement.Application.Contracts.Persistence;
+
+namespace HR.LeaveManagement.Application.Features.LeaveRequest.Shared;
+
+public class BaseLeaveRequestValidator : AbstractValidator<BaseLeaveRequest>
+{
+    private readonly ILeaveTypeRepository _leaveTypeRepository;
+
+    public BaseLeaveRequestValidator(ILeaveTypeRepository leaveTypeRepository)
+    {
+        this._leaveTypeRepository = leaveTypeRepository;
+
+        RuleFor(p => p.StartDate)
+            .LessThan(p => p.EndDate)
+            .WithMessage("{PropertyName} must be before {ComparisonValue}");
+
+        RuleFor(p => p.EndDate)
+            .GreaterThan(p => p.StartDate)
+            .WithMessage("{PropertyName} must be before {ComparisonValue}");
+
+        RuleFor(p => p.LeaveTypeId)
+            .GreaterThan(0)
+            .MustAsync(LeaveTypeMustExist)
+            .WithMessage("{PropertyName} does not exist.");
+    }
+
+    private async Task<bool> LeaveTypeMustExist(int id, CancellationToken cancellationToken = default)
+    {
+        var leaveType = await this._leaveTypeRepository.GetByIdAsync(id);
+        return leaveType != null;
+    }
+}
