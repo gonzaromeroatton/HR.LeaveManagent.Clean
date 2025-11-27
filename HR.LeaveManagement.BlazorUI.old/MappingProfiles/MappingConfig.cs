b2c42a9ccfe5c9f78240f889af8505d4d481036c
Gonza Romero Atton <148910080+gonzaromeroatton@users.noreply.github.com>
2025-11-27 11:29:23 -0300

    New UI proyect.

---
+++ HR.LeaveManagement.BlazorUI.old/MappingProfiles/MappingConfig.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using HR.LeaveManagement.BlazorUI.Modules;
+using HR.LeaveManagement.BlazorUI.Services.Base;
+
+namespace HR.LeaveManagement.BlazorUI.MappingProfiles;
+
+/// <summary>
+/// 
+/// </summary>
+public sealed class MappingConfig : Profile
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public MappingConfig()
+    {
+        CreateMap<LeaveTypeDto, LeaveTypeViewModel>().ReverseMap();
+        CreateMap<CreateLeaveTypeCommand, LeaveTypeViewModel>().ReverseMap();
+        CreateMap<UpdateLeaveTypeCommand, LeaveTypeViewModel>().ReverseMap();
+    }
+}
